7a8be766c288008b279b13d4232ac26c8e8a90e2
Paul T <4277512+pault-tech@users.noreply.github.com>
2025-01-19 22:19:43 +0000

    WIP

--- custom.el
+++ custom.el
@@ -437,14 +437,12 @@ See variable `server-auth-dir' for details."
 
 
 
-
-
-
-
-
 (setq vc-follow-symlinks t)
 (find-file "/workspaces/" )
 (find-file "/workspaces/*" 'wildcards)
 (find-file "/workspaces/*/a*build.sh" 'wildcards)
 (find-file "/workspaces/gh_utils/*.sh" 'wildcards)
 (setq vc-follow-symlinks 'ask)
+
+
+(load-file "/workspaces/gh_utils/custom.el")
