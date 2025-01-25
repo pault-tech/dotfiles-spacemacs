06eab09eaa8d16596772a3a25bc22fb714d6406f
Paul T <4277512+pault-tech@users.noreply.github.com>
2025-01-25 13:23:44 +0000

    fixes

--- custom.el
+++ custom.el
@@ -462,6 +462,21 @@ See variable `server-auth-dir' for details."
 
 
 
+(defun web-search-google (&optional word)
+  (interactive)
+  ;; (eww-browse-url
+  (browse-url
+   (concat
+    "http://www.google.com/search?q="
+    ;; "https://www.google.com/search?gbv=1&q="
+    ;; "https://duckduckgo.com/?t=ffab&q="
+    ;; (convert-string-to-url word)
+    (url-encode-url word)
+    )))
+(defun web-search-google-that ()
+  (interactive)
+  (web-search-google (thing-at-point-or-point-mark)))
+
 
 
 (setq vc-follow-symlinks t)
