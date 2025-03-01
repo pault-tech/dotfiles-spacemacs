d44cababe4853a0a046ca0ca1ff1d84a4de7d4ed
Paul T <4277512+pault-tech@users.noreply.github.com>
2025-03-01 02:41:05 +0000

    fixed

--- setup.sh
+++ setup.sh
@@ -74,7 +74,7 @@ git clone -b develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
 printf "\n\n(setq vterm-always-compile-module t)" >> ~/.emacs.d/early-init.el
 mkdir -p ~/.emacs.d/private/snippets/python-mode/
 cp ~/dotfiles-spacemacs/.emacs.d/private/snippets/python-mode/* \
-~/.emacs.d/private/snippets/python-mode/*
+~/.emacs.d/private/snippets/python-mode/
 #gnu screen
 sudo apt install -y screen
 
