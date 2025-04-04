2e52f8b3ad56da29792490f26f56db46d1004efa
Paul T <4277512+pault-tech@users.noreply.github.com>
2025-04-04 18:17:12 +0000

    wip

--- .emacs.d/private/snippets/python-mode/aa_install.sh
+++ .emacs.d/private/snippets/python-mode/aa_install.sh
@@ -1,2 +1,3 @@
 cp * /home/ubuntu/.emacs.d/private/snippets/python-mode/
-# cp * /workspaces/dotfiles-spacemacs/.emacs.d/private/snippets/python-mode/
+
+cp * /workspaces/dotfiles-spacemacs/.emacs.d/private/snippets/python-mode/
